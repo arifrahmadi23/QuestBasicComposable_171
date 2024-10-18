8fc53953f9a0bb868143e340e0e1a6f77c6f990b
arifrahmadi23 <muhammad231arif@gmail.com>
2024-10-18 20:49:57 +0700

    Mengubah tampilan Fotoku

--- app/src/main/java/com/example/tugas2praktikum/MainActivity.kt
+++ app/src/main/java/com/example/tugas2praktikum/MainActivity.kt
@@ -11,11 +11,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
@@ -84,6 +86,10 @@ fun HalamanLogin(modifier: Modifier = Modifier){
         Image(
             painter = painterResource(id = R.drawable.fotoku),
             contentDescription = "",
+            modifier = Modifier
+                .size(500.dp)
+                .clip(CircleShape)
+                .padding(8.dp)
         )
     }
 
