e6c4ff8bfca5ca41cfa2404542d3c32fe6e5b5fd
arifrahmadi23 <muhammad231arif@gmail.com>
2024-10-18 19:55:06 +0700

    Menambahkan Image "Logo Umy"

--- app/src/main/java/com/example/tugas2praktikum/MainActivity.kt
+++ app/src/main/java/com/example/tugas2praktikum/MainActivity.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -64,6 +67,10 @@ fun HalamanLogin(modifier: Modifier = Modifier){
             fontFamily = FontFamily.Serif)
         Spacer(modifier = Modifier.padding(5.dp))
 
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "",
+        )
     }
 
 }
