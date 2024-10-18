f152c53fe2af226dc720f7d04936c0036cb3b446
arifrahmadi23 <muhammad231arif@gmail.com>
2024-10-18 20:49:08 +0700

    Menambahkan fotoku

--- app/src/main/java/com/example/tugas2praktikum/MainActivity.kt
+++ app/src/main/java/com/example/tugas2praktikum/MainActivity.kt
@@ -80,6 +80,11 @@ fun HalamanLogin(modifier: Modifier = Modifier){
         Text("Muhammad Arif Fajri Rahmadi",
             fontSize = 20.sp,
             color = Color.Red)
+        Spacer(modifier = Modifier.padding(5.dp))
+        Image(
+            painter = painterResource(id = R.drawable.fotoku),
+            contentDescription = "",
+        )
     }
 
 }
